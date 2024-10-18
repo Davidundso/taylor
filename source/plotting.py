06eb2ebcfdf1a6f623bda069937c707b539edc16
David S <david.suckrow@student.uni-tuebingen.de>
2024-10-18 09:37:54 +0200

    created source folder

---
+++ source/plotting.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(name="source", packages=find_packages())
+
+# dictionary kopieren + umbenennen
+# dictionary überschreiben
+# aus anderen files importieren: from source.plotting import <<name dictionary>>
+# über rel width (als argument) größe des Plots anpassen
